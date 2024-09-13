b04c8ab8ad87a5a09bdcb4fbf5cd323df15e3ff2
Gavin Hunter <gavinmhunter.prime@gmail.com>
2024-09-13 12:01:50 -0600

    added(phase 5 tests)

---
+++ client/src/main/java/ui/Repl.java
@@ -0,0 +1,58 @@
+package ui;
+
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
+public class Repl {
+  private final ChessClient client;
+
+  public Repl() {
+    this.client=new ChessClient();
+  }
+
+  public void run() {
+    System.out.println("Welcome to chess! Log in to start.");
+    System.out.println(client.help());
+
+    var scanner=new Scanner(System.in);
+    var result="";
+    while (!result.equals("quit")) {
+      printPrompt();
+      String line=scanner.nextLine();
+
+      if (line.isEmpty()) line="clear;register user pass email;create game;list;join 1 WHITE;redraw";
+
+      var inputs=line.split(";");
+
+      for (var input : inputs) {
+        try {
+          result=client.eval(input);
+          System.out.println(SET_TEXT_COLOR_LIGHT_GREY + result);
+        } catch (Exception e) {
+          System.out.println(SET_TEXT_COLOR_RED + e.getMessage());
+        }
+      }
+    }
+  }
+
+  private void printPrompt() {
+    var color=switch (client.getState()) {
+      case SIGNED_OUT -> SET_TEXT_COLOR_RED;
+      default -> SET_TEXT_COLOR_GREEN;
+    };
+    var status=switch (client.getState()) {
+      case SIGNED_OUT -> "LOGGED OUT";
+      case SIGNED_IN -> "LOGGED IN";
+      case IN_GAME -> "IN GAME";
+      case OBSERVING -> "OBSERVING";
+    };
+    var fullStatus=color + "[" + status + "]";
+    System.out.print("\n" + RESET + fullStatus + RESET + " >>> " + SET_TEXT_COLOR_BLUE);
+  }
+
+  public void notify(String message) {
+    System.out.println(RESET + "\n" + SET_TEXT_COLOR_YELLOW + message + RESET);
+    printPrompt();
+  }
+}
--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,29 +1,151 @@
 package client;
 
+import models.AuthToken;
+import models.User;
 import org.junit.jupiter.api.*;
 import server.Server;
+import serverFacade.ServerFacade;
+import ui.ClientException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
 
+    private static ServerFacade facade=new ServerFacade();
+    private static User user;
+
     @BeforeAll
-    public static void init() {
+    public static void init() throws ClientException {
         server = new Server();
         var port = server.run(0);
+        facade = new ServerFacade(port);
+        user = new User("user", "pass", "email");
         System.out.println("Started test HTTP server on " + port);
     }
 
     @AfterAll
-    static void stopServer() {
+    static void stopServer() throws ClientException {
+        facade.clear();
         server.stop();
     }
 
+    @BeforeEach
+    void setup() throws ClientException {
+        facade.clear();
+    }
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    void clear() {
+        assertDoesNotThrow(() -> facade.clear());
+    }
+
+    @Test
+    void registerUserSuccess() {
+        var authToken=assertDoesNotThrow(() -> facade.registerUser(user));
+        assertNotNull(authToken);
+        assertEquals(user.username(), authToken.username());
+        assertNotNull(authToken.authToken());
+    }
+
+    @Test
+    void registerUserAlreadyExists() {
+        var authToken=assertDoesNotThrow(() -> facade.registerUser(user));
+        assertNotNull(authToken);
+        assertEquals(user.username(), authToken.username());
+        assertNotNull(authToken.authToken());
+        var ex=assertThrows(ClientException.class, () -> facade.registerUser(user));
+        assertEquals("Error: already taken", ex.getMessage());
+    }
+
+    @Test
+    void loginSuccess() {
+        var authToken=assertDoesNotThrow(() -> facade.registerUser(user));
+        assertNotNull(authToken);
+        assertEquals(user.username(), authToken.username());
+        assertNotNull(authToken.authToken());
+
+        var loginAuthToken=assertDoesNotThrow(() -> facade.login(user));
+        assertNotNull(loginAuthToken);
+        assertEquals(user.username(), loginAuthToken.username());
+        assertNotNull(loginAuthToken.authToken());
+
+        assertNotEquals(authToken.authToken(), loginAuthToken.authToken());
+    }
+
+    @Test
+    void loginUnauthorized() {
+        var ex=assertThrows(ClientException.class, () -> facade.login(user));
+        assertEquals("Error: unauthorized", ex.getMessage());
+    }
+
+    @Test
+    void logoutSuccess() {
+        var authToken=assertDoesNotThrow(() -> facade.registerUser(user));
+        assertDoesNotThrow(() -> facade.logout(authToken));
+        var ex=assertThrows(ClientException.class, () -> facade.listGames(authToken));
+        assertEquals("Error: unauthorized", ex.getMessage());
+    }
+
+    @Test
+    void logoutUnauthorized() {
+        var authToken=new AuthToken("user");
+        var ex=assertThrows(ClientException.class, () -> facade.logout(authToken));
+
+        assertEquals("Error: unauthorized", ex.getMessage());
+    }
+
+    @Test
+    void listGamesSuccess() {
+        var authToken=assertDoesNotThrow(() -> facade.registerUser(user));
+        assertNotNull(authToken);
+        assertEquals(user.username(), authToken.username());
+        assertNotNull(authToken.authToken());
+
+        var game=assertDoesNotThrow(() -> facade.createGame(authToken, "game"));
+        assertNotNull(game);
+        var games=assertDoesNotThrow(() -> facade.listGames(authToken));
+        assertNotNull(games);
+        assertEquals(games.getFirst().gameID(), game.gameID());
+    }
+
+    @Test
+    void listGamesUnauthorized() {
+        var authToken=new AuthToken("user");
+        var ex=assertThrows(ClientException.class, () -> facade.listGames(authToken));
+        assertEquals("Error: unauthorized", ex.getMessage());
+    }
+
+    @Test
+    void createGameSuccess() {
+        var authToken=assertDoesNotThrow(() -> facade.registerUser(user));
+        var game=assertDoesNotThrow(() -> facade.createGame(authToken, "game"));
+        assertNotNull(game);
+    }
+
+    @Test
+    void createGameUnauthorized() {
+        var authToken=new AuthToken("user");
+        var ex=assertThrows(ClientException.class, () -> facade.createGame(authToken, "game"));
+        assertEquals("Error: unauthorized", ex.getMessage());
+    }
+
+    @Test
+    void joinGameSuccess() {
+        var authToken=assertDoesNotThrow(() -> facade.registerUser(user));
+        var game=assertDoesNotThrow(() -> facade.createGame(authToken, "game"));
+        assertDoesNotThrow(() -> facade.joinGame(authToken, game.gameID(), "WHITE"));
+    }
+
+    @Test
+    void joinGameSlotTaken() {
+        var authToken=assertDoesNotThrow(() -> facade.registerUser(user));
+        var game=assertDoesNotThrow(() -> facade.createGame(authToken, "game"));
+        assertDoesNotThrow(() -> facade.joinGame(authToken, game.gameID(), "WHITE"));
+        var ex=assertThrows(ClientException.class, () -> facade.joinGame(authToken, game.gameID(), "WHITE"));
+        assertEquals("Error: already taken", ex.getMessage());
     }
 
 }
